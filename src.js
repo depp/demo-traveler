6202e00239bd2789d941dbe4d836cdb98da2f492
Dietrich Epp <depp@zdome.net>
2020-07-04 17:30:40 -0400

    Draw simple mountain range
    Size: 349

--- src.js
+++ src.js
@@ -1,9 +1,21 @@
-var zeroTime;
+z = 0;
+t = 'M-50,0';
+for (x = -50; x < 51; x++) {
+  t += ' L' + [x, Math.random() * 10];
+}
+t += ' L50,50 L-50,50 z';
+p = new Path2D(t);
 f = (time) => {
-  zeroTime = zeroTime || time;
-  time = ((time - zeroTime) / 5e3) % 1;
+  c.save();
+  c.translate(a.width / 2, a.height / 2);
+  c.scale(a.width * 0.01, a.width * 0.01);
+  z = z || time;
+  time = ((time - z) / 5e3) % 1;
   requestAnimationFrame(f);
   c.fillStyle = `rgb(${time * 255},${time * 255},${time * 255})`;
-  c.fillRect(0, 0, a.width, a.height);
+  c.fillRect(-50, -50, 100, 100);
+  c.fillStyle = '#060';
+  c.fill(p);
+  c.restore();
 };
 f();
