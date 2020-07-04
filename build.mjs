d0df62e546fc07c9f8fa07208a24cce0774d456e
Dietrich Epp <depp@zdome.net>
2020-07-04 12:54:20 -0400

    Print space stats when building

--- build.mjs
+++ build.mjs
@@ -2,6 +2,7 @@ import { mkdir, readFile, stat, writeFile } from 'fs/promises';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
+import chalk from 'chalk';
 import regpack from './RegPack/regPack.js';
 const { packer } = regpack;
 
@@ -96,13 +97,54 @@ function compressSource(source) {
   return { text, buf };
 }
 
+const barBlocks = Array(...Array(7).keys()).map((i) =>
+  String.fromCodePoint(0x2589 + 6 - i),
+);
+
+function makeBar(frac) {
+  const columns = Math.max(10, process.stderr.columns || 80) - 2;
+  const bwidth = Math.min(
+    columns * 8,
+    Math.max(0, Math.round(frac * columns * 8)),
+  );
+  const fullBlocks = bwidth >> 3;
+  const fracBlock = bwidth & 7;
+  let t = '';
+  t += '\u250c';
+  for (let j = 0; j < columns; j++) {
+    t += '\u2500';
+  }
+  t += '\u2510\n\u2502';
+  let i = 0;
+  for (; i < fullBlocks; i++) {
+    t += '\u2588';
+  }
+  if (fracBlock != 0) {
+    t += barBlocks[fracBlock];
+    i++;
+  }
+  for (; i < columns; i++) {
+    t += ' ';
+  }
+  t += '\u2502\n\u2514';
+  for (let j = 0; j < columns; j++) {
+    t += '\u2500';
+  }
+  t += '\u2518\n';
+  return t;
+}
+
 function printStats(size) {
   process.stderr.write(`Size: ${size} bytes\n`);
   if (size > sizeLimit) {
     const frac = (size - sizeLimit) / sizeLimit;
-    process.stderr.write(`Over limit by: ${(100 * frac).toFixed(1)}%\n`);
+    process.stderr.write(chalk.red.bgWhiteBright(makeBar(frac)));
+    process.stderr.write(
+      chalk.redBright(`Over limit by: ${(100 * frac).toFixed(1)}%\n`),
+    );
   } else {
     const frac = (sizeLimit - size) / sizeLimit;
+    process.stderr.write(chalk.greenBright.bgBlue(makeBar(frac)));
     process.stderr.write(`Space remaining: ${(100 * frac).toFixed(1)}%\n`);
   }
 }
