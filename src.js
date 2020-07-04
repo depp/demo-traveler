43054ff5a5e6142d457680b0b2532ce63a93d159
Dietrich Epp <depp@zdome.net>
2020-07-04 15:10:13 -0400

    Animate background color

--- src.js
+++ src.js
@@ -1,2 +1,9 @@
-c.fillStyle = '#c52';
-c.fillRect(0, 0, a.width, a.height);
+var z;
+f = (t) => {
+  z = z || t;
+  t = ((t - z) / 5e3) % 1;
+  requestAnimationFrame(f);
+  c.fillStyle = 'rgb(' + t * 255 + ',' + t * 255 + ',' + t * 255 + ')';
+  c.fillRect(0, 0, a.width, a.height);
+};
+f();
