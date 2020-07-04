ef4b8ba72b473e38f7dc5c8ca3fa4b2196156a5d
Dietrich Epp <depp@zdome.net>
2020-07-04 15:29:56 -0400

    Use template literal
    Size: 174

--- src.js
+++ src.js
@@ -3,7 +3,7 @@ f = (t) => {
   z = z || t;
   t = ((t - z) / 5e3) % 1;
   requestAnimationFrame(f);
-  c.fillStyle = 'rgb(' + t * 255 + ',' + t * 255 + ',' + t * 255 + ')';
+  c.fillStyle = `rgb(${t * 255},${t * 255},${t * 255})`;
   c.fillRect(0, 0, a.width, a.height);
 };
 f();
