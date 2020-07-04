835d65ac5d232ced45bc186d8ac93102dac4fe8c
Dietrich Epp <depp@zdome.net>
2020-07-04 14:48:08 -0400

    Add server with automatic reloading

--- build.mjs
+++ build.mjs
@@ -1,10 +1,16 @@
 import { mkdir, readFile, stat, writeFile } from 'fs/promises';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+import http from 'http';
 
 import chalk from 'chalk';
+import chokidar from 'chokidar';
+import express from 'express';
 import regpack from './RegPack/regPack.js';
 const { packer } = regpack;
+import send from 'send';
+import WebSocket from 'ws';
+import yargs from 'yargs';
 
 // Title of the demo.
 const title = 'Traveler';
@@ -120,7 +126,7 @@ function makeBar(frac) {
     t += '\u2588';
   }
   if (fracBlock != 0) {
-    t += barBlocks[fracBlock];
+    t += barBlocks[fracBlock - 1];
     i++;
   }
   for (; i < columns; i++) {
@@ -134,7 +140,9 @@ function makeBar(frac) {
   return t;
 }
 
-function printStats(size) {
+function printStats(result) {
+  const { source } = result;
+  const size = source.length;
   process.stderr.write(`Size: ${size} bytes\n`);
   if (size > sizeLimit) {
     const frac = (size - sizeLimit) / sizeLimit;
@@ -155,19 +163,184 @@ async function build() {
     readFile(path('shim.html'), 'utf8'),
   ]);
   const { text, buf } = compressSource(source);
-  printStats(buf.length);
   const html = buildHTML(text, shim);
   await Promise.all([
     writeFile(path('build', 'demo.js'), buf),
     writeFile(path('build', 'demo.html'), html, 'utf8'),
   ]);
+  return {
+    source: buf,
+    html,
+  };
 }
+
+async function watch(callback) {
+  let markDirty = null;
+  chokidar.watch([path('src.js'), path('shim.html')]).on('all', () => {
+    if (markDirty != null) {
+      setTimeout(markDirty, 100);
+      markDirty = null;
+    }
+  });
+  while (true) {
+    await new Promise((r) => {
+      markDirty = r;
+    });
+    try {
+      callback(await build(), null);
+    } catch (e) {
+      callback(null, e);
+    }
+  }
+}
+
+async function buildCommand(args) {
+  printStats(await build());
+}
+
+function watchCommand(args) {
+  return watch((result, e) => {
+    if (e != null) {
+      console.error(e);
+    } else {
+      printStats(result);
+    }
+  });
+}
+
+function equalObj(x, y) {
+  for (const k in x) {
+    if (!(k in y) || x[k] !== y[k]) {
+      return false;
+    }
+  }
+  for (const k in y) {
+    if (!(k in x)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function serveCommand(args) {
+  const { _, host, port } = yargs
+    .options({
+      'port': { type: 'number', default: 7000, desc: 'Serve on this port' },
+      'host': {
+        type: 'string',
+        default: 'localhost',
+        desc: 'Serve on this host',
+      },
+    })
+    .strict()
+    .parse(args);
+  if (_.length != 0) {
+    die(`Unexpected argument: ${JSON.stringify(_[0])}`);
+  }
+  const app = express();
+  function staticFile(url, relPath) {
+    const fullPath = path(relPath);
+    app.get(url, function handler(req, res) {
+      res.setHeader('Cache-Control', 'no-cache');
+      send(req, fullPath, {
+        cacheControl: false,
+      }).pipe(res);
+    });
+  }
+  staticFile('/', 'frame.html');
+  staticFile('/reload.js', 'reload.js');
+  staticFile('/style.css', 'style.css');
+  app.get('/demo.html', function (req, res) {
+    res.setHeader('Cache-Control', 'no-cache');
+    const data = html;
+    if (data == null) {
+      res.status(500).send('<h1>Not ready</h1>');
+    } else {
+      res.status(200).send(data);
+    }
+  });
+  let status = { state: 'dirty' };
+  let html = null;
+  function setStatus(newStatus) {
+    if (equalObj(status, newStatus)) {
+      return;
+    }
+    status = newStatus;
+    for (const ws of sockets) {
+      ws.send(JSON.stringify(['status', newStatus]));
+    }
+  }
+  function setHTML(newHTML) {
+    if (html === newHTML) {
+      return;
+    }
+    html = newHTML;
+    for (const ws of sockets) {
+      ws.send(JSON.stringify(['changed']));
+    }
+  }
+  const server = http.createServer(app);
+  const sockets = [];
+  function removeSocket(ws) {
+    const index = sockets.find((e) => ws === e);
+    if (index !== -1) {
+      sockets.splice(index, 1);
+    }
+  }
+  function handleWebSocket(ws) {
+    sockets.push(ws);
+    ws.on('close', () => removeSocket(ws));
+    ws.send(JSON.stringify(['status', status]));
+  }
+  const wss = new WebSocket.Server({ server });
+  wss.on('connection', handleWebSocket);
+  await new Promise((resolve) => {
+    server.listen(port, host, () => {
+      process.stderr.write(`Listening on http://${host}:${port}/\n`);
+      resolve();
+    });
+  });
+  await watch((result, err) => {
+    if (err != null) {
+      console.error(err);
+      setStatus({ state: 'error', message: err.message });
+    } else {
+      const { source, html } = result;
+      setStatus({ state: 'ok', size: source.length, sizeLimit });
+      setHTML(html);
+    }
+  });
+}
+
+function die(msg) {
+  process.stderr.write(`Error: ${msg}\n`);
+  process.exit(1);
+}
+
+function usage() {
+  process.stdout.write('Usage: node build.mjs {build|serve} [option...]\n');
+}
+
+const commands = new Map([
+  ['build', buildCommand],
+  ['watch', watchCommand],
+  ['serve', serveCommand],
+]);
 
 async function main() {
   try {
-    const buildDir = path('build');
-    await mkdirExistOk(buildDir);
-    await build();
+    const args = process.argv.slice(2);
+    if (args.length == 0) {
+      usage();
+      return;
+    }
+    const [cmdName, ...cmdArgs] = args;
+    const cmdFunc = commands.get(cmdName);
+    if (cmdFunc == null) {
+      die(`Unknown command: ${JSON.stringify(cmd)}`);
+    }
+    await mkdirExistOk(path('build'));
+    await cmdFunc(cmdArgs);
   } catch (e) {
     console.error(e);
     process.exit(1);
---
+++ reload.js
@@ -0,0 +1,91 @@
+// =============================================================================
+// Status
+// =============================================================================
+
+const statusElt = document.getElementById('status');
+const frameElt = document.getElementById('frame');
+
+function setStatus(className, msg) {
+  const p = document.createElement('p');
+  p.className = className;
+  p.appendChild(document.createTextNode(msg));
+  while (statusElt.lastChild != null) {
+    statusElt.removeChild(statusElt.lastChild);
+  }
+  statusElt.append(p);
+}
+
+setStatus('working', 'Connecting');
+
+// =============================================================================
+// Web Socket
+// =============================================================================
+
+const ws = new WebSocket(`ws://${window.location.host}/socket`);
+ws.addEventListener('error', wsError);
+ws.addEventListener('open', wsOpen);
+ws.addEventListener('close', wsClose);
+ws.addEventListener('message', wsMessage);
+
+function wsError(ev) {
+  setStatus('error', 'Disconnected');
+}
+
+function wsOpen(ev) {
+  setStatus('working', 'Connected');
+}
+
+function wsClose(ev) {
+  setStatus('error', 'Disconnected');
+}
+
+function wsMessage(ev) {
+  const [name, ...value] = JSON.parse(ev.data);
+  switch (name) {
+    case 'status':
+      statusMessage(value[0]);
+      break;
+    case 'changed':
+      changedMessage();
+      break;
+    default:
+      console.error(`Unknown message:`, ev.data);
+      break;
+  }
+}
+
+function statusMessage(msg) {
+  switch (msg.state) {
+    case 'dirty':
+      setStatus('working', 'Dirty');
+      break;
+    case 'ok':
+      {
+        const { size, sizeLimit } = msg;
+        if (size <= sizeLimit) {
+          const pct = (100 * (sizeLimit - size)) / sizeLimit;
+          setStatus(
+            'ok',
+            `Ok: ${size}/${sizeLimit} bytes, ${pct.toFixed(1)}% remaining`,
+          );
+        } else {
+          const pct = (100 * (size - sizeLimit)) / sizeLimit;
+          setStatus(
+            'error',
+            `Error: ${size}/${sizeLimit} bytes, ${pct.toFixed(1)}% over`,
+          );
+        }
+      }
+      break;
+    case 'error':
+      setStatus('error', 'Error: ' + msg.message);
+      break;
+    default:
+      console.error(`Unknown status:`, msg);
+      break;
+  }
+}
+
+function changedMessage() {
+  frameElt.contentWindow.location.reload();
+}
